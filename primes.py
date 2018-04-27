529e52c66c922850aa0fa29d2ff52791ebe48a73
sharpvik <sharp.vik@gmail.com>
2018-04-27 00:11:54 -0400

    push test

--- primes.py
+++ primes.py
@@ -103,3 +103,4 @@ def find_prime_factors(n):                          # n(int) -- any positive num
         prime_factors.append(int(num_to_test))      # when num_to_test becomes prime itself we put it into the
     return prime_factors
 ## find prime factors <--
+#
